a2fe51a955a13807d2c949f887a066ac722615a4
Jan Szczepan <janszczepan62@gmail.com>
2023-05-02 20:47:27 +0200

    Deploy contract script

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,26 @@
-async function main() {}
+import { ethers } from 'ethers'
+import * as fs from 'fs-extra'
+import 'dotenv/config'
+
+async function main() {
+   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL)
+   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+
+   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8')
+   const binary = fs.readFileSync(
+      './SimpleStorage_sol_SimpleStorage.bin',
+      'utf8'
+   )
+
+   const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+
+   console.log('Deploying, please wait...')
+
+   const contract = await contractFactory.deploy()
+   await contract.deploymentTransaction()?.wait(1)
+
+   console.log(`Contract deployed to: ${await contract.getAddress()}`)
+}
 
 main()
    .then(() => process.exit(0))
